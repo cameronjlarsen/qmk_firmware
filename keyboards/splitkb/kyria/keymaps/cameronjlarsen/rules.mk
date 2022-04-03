e6b37123ae07ba353b0f6a5cde8aada032dac263
Cameron Larsen <cameronjlarsen@gmail.com>
2022-04-03 02:23:42 -0700

    Added bootloader and mcu options to rules.mk

--- keyboards/splitkb/kyria/keymaps/cameronjlarsen/rules.mk
+++ keyboards/splitkb/kyria/keymaps/cameronjlarsen/rules.mk
@@ -1,17 +1,25 @@
 SRC += features/caps_word.c
 SRC += features/oneshot.c
 
-BOOTMAGIC_ENABLE = no
+# MCU name
+MCU = atmega32u4
+
+#Bootloader selection
+BOOTLOADER = atmel-dfu
+
+#Build options
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
-COMMAND_ENABLE = no # Disables the command feature
 COMBO_ENABLE = yes
-MOUSEKEY_ENABLE = no
-CONSOLE_ENABLE = no
-AUDIO_ENABLE = no
+SPLIT_KEYBOARD = yes
+COMMAND_ENABLE = no # Commands for debug and configuration
+BOOTMAGIC_ENABLE = no # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no # Mouse keys
+CONSOLE_ENABLE = no # Console for debug
+AUDIO_ENABLE = no # Audio control and System control
 MIDI_ENABLE = no
 BLUETOOTH_ENABLE = no
-BACKLIGHT_ENABLE = no
+BACKLIGHT_ENABLE = no # Enable keyboard backlight functionality
 
